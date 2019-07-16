bd9752081982dff3574a7c5a6192f5d6cb56804b
lykmapipo <lallyelias87@gmail.com>
2019-07-16 12:03:27 +0300

    feat: add query keys and create helpers

--- library/src/main/java/com/github/lykmapipo/listview/data/Query.java
+++ library/src/main/java/com/github/lykmapipo/listview/data/Query.java
@@ -2,6 +2,8 @@ package com.github.lykmapipo.listview.data;
 
 import android.text.TextUtils;
 
+import androidx.annotation.NonNull;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,6 +14,15 @@ import java.util.Map;
  * @since 0.1.0
  */
 public class Query {
+    // query map data keys
+    public static final String KEY_SEARCH = "q";
+    public static final String KEY_PAGE = "page";
+    public static final String KEY_LIMIT = "limit";
+    public static final String KEY_FILTER = "filter";
+    public static final String KEY_FIELDS = "fields";
+    public static final String KEY_SORT = "sort";
+    public static final String KEY_POPULATE = "populate";
+
     // specific search query condition
     String q;
 
@@ -22,14 +33,47 @@ public class Query {
     Long limit = 10L;
 
     /**
-     * Instantiate default query
+     * Instantiate default {@link Query}
      *
      * @return {@link Query}
      * @since 0.1.0
      */
+    @NonNull
     public static synchronized Query create() {
         Query query = new Query();
         query.page(1L);
+        query.limit(10L);
+        return query;
+    }
+
+    /**
+     * Instantiate new {@link Query}
+     *
+     * @param page valid page
+     * @return {@link Query}
+     * @since 0.1.0
+     */
+    @NonNull
+    public static synchronized Query create(@NonNull Long page) {
+        Query query = new Query();
+        query.page(page);
+        query.limit(10L);
+        return query;
+    }
+
+    /**
+     * Instantiate new {@link Query}
+     *
+     * @param q    valid search term
+     * @param page valid page
+     * @return {@link Query}
+     * @since 0.1.0
+     */
+    @NonNull
+    public static synchronized Query create(@NonNull String q, @NonNull Long page) {
+        Query query = new Query();
+        query.search(q);
+        query.page(page);
         query.limit(10L);
         return query;
     }
@@ -40,6 +84,7 @@ public class Query {
      * @return {@link Map}
      * @since 0.1.0
      */
+    @NonNull
     public static synchronized Map<String, String> defaultQueryMap() {
         Query query = create();
         return query.toQueryMap();
@@ -51,20 +96,21 @@ public class Query {
      * @return {@link Map}
      * @since 0.1.0
      */
+    @NonNull
     public Map<String, String> toQueryMap() {
         // initialize query map
         HashMap<String, String> queryMap = new HashMap<String, String>();
 
         // handle search query condition
         if (!TextUtils.isEmpty(q)) {
-            queryMap.put("q", q);
+            queryMap.put(KEY_SEARCH, q);
         }
 
         // handle page condition
-        queryMap.put("page", String.valueOf(page == null ? 1L : page));
+        queryMap.put(KEY_PAGE, String.valueOf(page == null ? 1L : page));
 
         // handle limit condition
-        queryMap.put("limit", String.valueOf(limit == null ? 10L : limit));
+        queryMap.put(KEY_LIMIT, String.valueOf(limit == null ? 10L : limit));
 
         // return query map
         return queryMap;
@@ -77,7 +123,8 @@ public class Query {
      * @return {@link Query}
      * @since 0.1.0
      */
-    public Query search(String q) {
+    @NonNull
+    public Query search(@NonNull String q) {
         this.q = q;
         return this;
     }
@@ -89,7 +136,8 @@ public class Query {
      * @return {@link Query}
      * @since 0.1.0
      */
-    public Query page(Long page) {
+    @NonNull
+    public Query page(@NonNull Long page) {
         this.page = page;
         return this;
     }
@@ -100,7 +148,8 @@ public class Query {
      * @param limit valid page number
      * @return {@link Query}
      */
-    public Query limit(Long limit) {
+    @NonNull
+    public Query limit(@NonNull Long limit) {
         this.limit = limit;
         return this;
     }
