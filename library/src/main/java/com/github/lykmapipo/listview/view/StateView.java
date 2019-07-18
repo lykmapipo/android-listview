71a356e6e9934b2ccfa5063a6b410ca869fa47fc
lykmapipo <lallyelias87@gmail.com>
2019-07-18 11:57:10 +0300

    feat: obtain state view style attributes for appy

--- library/src/main/java/com/github/lykmapipo/listview/view/StateView.java
+++ library/src/main/java/com/github/lykmapipo/listview/view/StateView.java
@@ -1,8 +1,11 @@
 package com.github.lykmapipo.listview.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
+
+import com.github.lykmapipo.listview.R;
 
 /**
  * A StateView is a derivative of {@link android.widget.LinearLayout} used to displays
@@ -12,6 +15,13 @@ import android.widget.LinearLayout;
  * @since 0.1.0
  */
 public class StateView extends LinearLayout {
+
+    private int titleResId = R.string.text_state_view_title;
+    private int messageResId = R.string.text_state_view_message;
+    private int imageSrcResId = R.drawable.ic_empty;
+    private int imageWidthResId = R.dimen.layout_width_state_view_image_width;
+    private int imageHeightResId = R.dimen.layout_width_state_view_image_height;
+    private int actionTextResId = R.string.text_state_view_action_text;
 
     public StateView(Context context) {
         this(context, null);
@@ -23,5 +33,23 @@ public class StateView extends LinearLayout {
 
     public StateView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.StateView);
+        // obtain attributes
+        try {
+            titleResId = ta.getResourceId(R.styleable.StateView_title, titleResId);
+            messageResId = ta.getResourceId(R.styleable.StateView_message, messageResId);
+            imageSrcResId = ta.getResourceId(R.styleable.StateView_image_src, imageSrcResId);
+            imageWidthResId = ta.getResourceId(R.styleable.StateView_image_width, imageWidthResId);
+            imageHeightResId = ta.getResourceId(R.styleable.StateView_image_height, imageHeightResId);
+            actionTextResId = ta.getResourceId(R.styleable.StateView_action_text, actionTextResId);
+        }
+        // recycle TypedArray
+        finally {
+            ta.recycle();
+        }
     }
 }
